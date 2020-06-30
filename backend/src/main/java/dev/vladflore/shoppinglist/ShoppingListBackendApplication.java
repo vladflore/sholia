60967fbda0fa70d5d6ea3330c50537600cff62f9
Vlad Flore <flore.vlad@gmail.com>
2020-06-30 23:40:48 +0200

    set up backend and initial project structure

---
+++ backend/src/main/java/dev/vladflore/shoppinglist/ShoppingListBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.vladflore.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/vladflore/shoppinglist/ShoppingListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vladflore.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
