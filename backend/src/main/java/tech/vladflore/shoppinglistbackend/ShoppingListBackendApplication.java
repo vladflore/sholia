0a9884f95e40b1e478ffb9f8929d4bce443316a1
Vlad Flore <flore.vlad@gmail.com>
2020-07-06 16:02:15 +0200

    rename package and pom coordinates

--- backend/src/main/java/tech/vladflore/shoppinglistbackend/ShoppingListBackendApplication.java
+++ backend/src/main/java/tech/vladflore/shoppinglistbackend/ShoppingListBackendApplication.java
@@ -1,4 +1,4 @@
-package dev.vladflore.shoppinglist;
+package tech.vladflore.shoppinglistbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/tech/vladflore/shoppinglistbackend/ShoppingListBackendApplicationTests.java
+++ backend/src/test/java/tech/vladflore/shoppinglistbackend/ShoppingListBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.vladflore.shoppinglist;
+package tech.vladflore.shoppinglistbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
