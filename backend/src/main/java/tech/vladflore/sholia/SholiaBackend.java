e2d14df15d43491ff7efacd0a2cbad3c61f545bf
Vlad Flore <flore.vlad@gmail.com>
2021-01-21 18:37:55 +0100

    Remove superfluous javadoc

--- backend/src/main/java/tech/vladflore/sholia/SholiaBackend.java
+++ backend/src/main/java/tech/vladflore/sholia/SholiaBackend.java
@@ -3,11 +3,6 @@ package tech.vladflore.sholia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point in the application.
- *
- * @author Vlad Flore
- */
 @SpringBootApplication
 public class SholiaBackend {
 
